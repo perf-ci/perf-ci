d73ee3ab25b366b63c88d4fcabc8368563e06ad2
Aleksey Timin <atimin@gmail.com>
2020-05-31 22:08:15 +0700

    log info

--- api/auth/github.py
+++ api/auth/github.py
@@ -1,7 +1,10 @@
+import logging
+
 from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
 from starlette.responses import RedirectResponse, JSONResponse
-from logging import debug
+
+logger = logging.getLogger('github')
 
 config = Config(".env")
 oauth = OAuth(config)
@@ -10,7 +13,7 @@ github = oauth.register('github')
 
 async def login(request):
     redirect_uri = request.url_for('github_auth')
-    debug(f'Redirect URL {redirect_uri}')
+    logger.info(f'Redirect URL {redirect_uri}')
     return await oauth.github.authorize_redirect(request, redirect_uri)
 
 
